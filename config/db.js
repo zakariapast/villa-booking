41d77385a277161fb049bae324fbf67b83e58f3c
zakariapast <37060701+zakariapast@users.noreply.github.com>
2025-05-18 23:52:46 +0800

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 // config/db.js
+
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
 const uri = process.env.MONGO_URI;
 const client = new MongoClient(uri);
@@ -9,7 +11,7 @@ let db;
 async function connectToDB() {
   if (!db) {
     await client.connect();
-    db = client.db('villa_booking');
+    db = client.db('villa_booking'); // You can name your DB anything
   }
   return db;
 }
